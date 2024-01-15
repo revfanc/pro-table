bb775e3d17568300defb45b7cf2d0b2a1ac9963c
fj517 <fj517@yoc.tech>
2024-01-15 14:38:23 +0800

    feat: init

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,9 @@ module.exports = {
     node: true
   },
   extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
+    'plugin:vue/recommended',
+    '@vue/standard',
+    'plugin:json/recommended'
   ],
   parserOptions: {
     parser: 'babel-eslint'
